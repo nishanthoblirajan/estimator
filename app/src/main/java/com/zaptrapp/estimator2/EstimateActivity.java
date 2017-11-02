439a61f54f9dd5ea4456cb107894ebe1b8b5c412
Obli Nishanth Oblirajan <obli.oblirajan.13@ucl.ac.uk>
2017-11-03 00:12:11 +0530

    firebase working

--- app/src/main/java/com/zaptrapp/estimator2/EstimateActivity.java
+++ app/src/main/java/com/zaptrapp/estimator2/EstimateActivity.java
@@ -1,13 +1,19 @@
 package com.zaptrapp.estimator2;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.telecom.Call;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -15,11 +21,16 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.zaptrapp.estimator2.Models.Product;
+import com.zaptrapp.estimator2.Models.ProductHolder;
 import com.zaptrapp.estimator2.Models.VA;
 
 public class EstimateActivity extends AppCompatActivity {
@@ -35,7 +46,6 @@ public class EstimateActivity extends AppCompatActivity {
     double greaterThanSix;
 
 
-
     public static final String TAG = EstimateActivity.class.getSimpleName();
 
     private EditText etGramRate;
@@ -48,6 +58,7 @@ public class EstimateActivity extends AppCompatActivity {
     private RadioButton rbKdm;
     private Button btEstimate;
     private TextView tvEstimateOut;
+    private RecyclerView recyclerView;
 
     //View initalization
     private void initView() {
@@ -61,17 +72,21 @@ public class EstimateActivity extends AppCompatActivity {
         rbKdm = (RadioButton) findViewById(R.id.rb_kdm);
         btEstimate = (Button) findViewById(R.id.bt_estimate);
         tvEstimateOut = (TextView) findViewById(R.id.tv_estimate_out);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estimate);
         initDatabase();
         initView();
+        initRecycler();
     }
 
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
+
     private void initDatabase() {
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference("estimator2");
@@ -87,14 +102,58 @@ public class EstimateActivity extends AppCompatActivity {
     }
 
     private void retrieveDataFromFirebase(String product_estimate) {
-        Log.d(TAG, "retrieveDataFromFirebase: "+product_estimate);
-        DatabaseReference databaseReference1 = firebaseDatabase.getReference("estimator2/VA/"+product_estimate);
+        Log.d(TAG, "retrieveDataFromFirebase: " + product_estimate);
+        DatabaseReference databaseReference1 = firebaseDatabase.getReference("estimator2/VA/" + product_estimate);
         databaseReference1.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 VA valueAdded = dataSnapshot.getValue(VA.class);
-                Log.d(TAG,valueAdded.toString());
+                Log.d(TAG, valueAdded.toString());
                 initVAs(valueAdded);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                initVAs();
+            }
+        });
+    }
+
+    //onCancelled
+    private void initVAs() {
+        lessThanOne = 0;
+        one = 0;
+        two = 0;
+        three = 0;
+        four = 0;
+        five = 0;
+        six = 0;
+        greaterThanSix = 0;
+    }
+
+    //on receiving the value from the Firebase Database
+    private void initVAs(VA valueAdded) {
+        lessThanOne = valueAdded.getLessThanOne();
+        one = valueAdded.getOne();
+        two = valueAdded.getTwo();
+        three = valueAdded.getThree();
+        four = valueAdded.getFour();
+        five = valueAdded.getFive();
+        six = valueAdded.getSix();
+        greaterThanSix = valueAdded.getGreaterThanSix();
+    }
+
+
+    FirebaseRecyclerAdapter<Product, ProductHolder> adapter;
+
+    private void initRecycler() {
+        Log.d(TAG, "initRecycler: ");
+        Query query = databaseReference.child("gold");
+        Log.d(TAG, "initRecycler: " + query.getRef());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.d(TAG, "onDataChange: "+dataSnapshot.getValue().toString());
             }
 
             @Override
@@ -102,20 +161,36 @@ public class EstimateActivity extends AppCompatActivity {
 
             }
         });
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+        FirebaseRecyclerOptions<Product> productOptions =
+                new FirebaseRecyclerOptions.Builder<Product>()
+                .setQuery(query,Product.class)
+                .build();
+        adapter = new FirebaseRecyclerAdapter<Product, ProductHolder>(productOptions) {
+            @Override
+            protected void onBindViewHolder(ProductHolder holder, int position, Product model) {
+                Log.d(TAG, "initRecycler onBindViewHolder: ");
+                holder.product.setText(model.getProductName());
+            }
+
+            @Override
+            public ProductHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                Log.d(TAG, "initRecycler onCreateViewHolder: ");
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.recycler_items,parent,false);
+                return new ProductHolder(view);
+            }
+        };
+
+        recyclerView.setAdapter(adapter);
     }
-
-    private void initVAs(VA valueAdded) {
-        double lessThanOne;
-        double one;
-        double two;
-        double three;
-        double four;
-        double five;
-        double six;
-        double greaterThanSix;
-
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
     }
-
     private String goldOrSilver() {
         int product_id = rgProduct.getCheckedRadioButtonId();
         String product = "Error";
@@ -132,11 +207,12 @@ public class EstimateActivity extends AppCompatActivity {
         }
         return product;
     }
+
     //onOptionsmenuCreated
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.main_menu,menu);
+        menuInflater.inflate(R.menu.main_menu, menu);
         return true;
     }
 
@@ -144,9 +220,9 @@ public class EstimateActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.menu_add:
-                startActivity(new Intent(this,AddProductActivity.class));
+                startActivity(new Intent(this, AddProductActivity.class));
                 return true;
             default:
                 return false;
---
+++ app/src/main/java/com/zaptrapp/estimator2/Models/ProductHolder.java
@@ -0,0 +1,18 @@
+package com.zaptrapp.estimator2.Models;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by Nishanth on 02-Nov-17.
+ */
+
+public class ProductHolder extends RecyclerView.ViewHolder {
+    public final TextView product;
+    public ProductHolder(View itemView) {
+        super(itemView);
+        product = itemView.findViewById(android.R.id.text1);
+    }
+
+}
