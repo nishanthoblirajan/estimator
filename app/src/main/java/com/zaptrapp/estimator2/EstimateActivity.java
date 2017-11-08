d94001c763f0e3ae6fcde955e7fc39bf85285d0b
Obli Nishanth Oblirajan <obli.oblirajan.13@ucl.ac.uk>
2017-11-08 22:08:27 +0530

    added printer library

--- app/src/main/java/com/zaptrapp/estimator2/EstimateActivity.java
+++ app/src/main/java/com/zaptrapp/estimator2/EstimateActivity.java
@@ -1,7 +1,10 @@
 package com.zaptrapp.estimator2;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -26,6 +29,10 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.epson.epos2.Epos2Exception;
+import com.epson.epos2.printer.Printer;
+import com.epson.epos2.printer.PrinterStatusInfo;
+import com.epson.epos2.printer.ReceiveListener;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DataSnapshot;
@@ -39,17 +46,20 @@ import com.zaptrapp.estimator2.Models.CreateEstimate;
 import com.zaptrapp.estimator2.Models.Product;
 import com.zaptrapp.estimator2.Models.ProductHolder;
 import com.zaptrapp.estimator2.Models.VA;
+import com.zaptrapp.estimator2.Printer.ShowMsg;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import io.reactivex.functions.Consumer;
 
-public class EstimateActivity extends AppCompatActivity {
+public class EstimateActivity extends AppCompatActivity implements ReceiveListener {
 
-
+    public String PRINTER = "BT:00:01:90:C2:AE:35";
     public static final String TAG = EstimateActivity.class.getSimpleName();
     SharedPreferences sharedPreferences;
     FirebaseDatabase firebaseDatabase;
@@ -137,6 +147,7 @@ public class EstimateActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estimate);
 
+        mContext = this;
         buyingItem = false;
         initVAs();
         initDatabase();
@@ -474,6 +485,7 @@ public class EstimateActivity extends AppCompatActivity {
             Log.d(TAG, "Estimate List: "+mCreateEstimateList.get(i).toString());
         }
 
+
         //if all check out log the estimate copy to the logcat window
 
     }
@@ -506,7 +518,6 @@ public class EstimateActivity extends AppCompatActivity {
     //TODO implement send to printer
     private void sendToPrinter(CreateEstimate createEstimate) {
         String printer  = createEstimate.printer;
-
 
     }
 
@@ -709,4 +720,300 @@ public class EstimateActivity extends AppCompatActivity {
     }
 
 
+
+    Printer mPrinter;
+    Context mContext = null;
+
+    /*
+    CAUTION: PROCEED WITH CARE
+     */
+
+    private boolean runPrintReceiptSequence(String printString) {
+        Log.d(TAG, "runPrintReceiptSequence: ");
+        try {
+
+            if (!initializeObject()) {
+                Log.d(TAG, "runPrintReceiptSequence: initalise");
+                return false;
+            }
+
+            if (!createReceiptData(printString)) {
+                Log.d(TAG, "runPrintReceiptSequence: createre");
+                finalizeObject();
+                return false;
+            }
+
+            if (!printData()) {
+                Log.d(TAG, "runPrintReceiptSequence: ");
+                finalizeObject();
+                return false;
+            }
+        } catch (Exception e) {
+            Toast.makeText(mContext, "Print Failed, Try Again", Toast.LENGTH_SHORT).show();
+        }
+        return true;
+
+    }
+
+    private boolean initializeObject() {
+        Log.d(TAG, "initializeObject: ");
+        try {
+            mPrinter = new Printer(Printer.TM_M30,
+                    Printer.MODEL_ANK,
+                    this);
+            Log.d(TAG, "initializeObject: inside try");
+        } catch (Exception e) {
+            Log.d(TAG, "initializeObject: Exception");
+            ShowMsg.showException(e, "Printer", mContext);
+            return false;
+        }
+
+        mPrinter.setReceiveEventListener((ReceiveListener) mContext);
+
+        return true;
+    }
+
+
+    private boolean createReceiptData(String printString) {
+        Log.d(TAG, "createReceiptData: ");
+
+        if (mPrinter == null) {
+            return false;
+        }
+
+        try {
+            mPrinter.addTextAlign(Printer.ALIGN_LEFT);
+            String dateStamp = new SimpleDateFormat("dd/MM/yy").format(new Date());
+            String timeStamp = new SimpleDateFormat("HH:mm:ss").format(new Date());
+            mPrinter.addText("Date: " + dateStamp);
+            mPrinter.addFeedLine(1);
+            mPrinter.addTextSize(2, 2);
+            mPrinter.addTextAlign(Printer.ALIGN_CENTER);
+            mPrinter.addFeedLine(1);
+            mPrinter.addText("ESTIMATE");
+            mPrinter.addFeedLine(2);
+            mPrinter.addTextSize(1, 1);
+            mPrinter.addText(printString.split("_")[0]);
+            mPrinter.addText("------------------------------------------\n");
+            mPrinter.addFeedLine(1);
+            mPrinter.addTextSize(2, 2);
+            mPrinter.addText("Rs " + printString.split("_")[1]);
+            mPrinter.addFeedLine(3);
+            mPrinter.addTextSize(1, 1);
+            mPrinter.addText("------------------------------------------\n");
+            mPrinter.addText("Thank You\n");
+            mPrinter.addTextAlign(Printer.ALIGN_RIGHT);
+            mPrinter.addFeedLine(4);
+            mPrinter.addText("Time " + timeStamp + "\n");
+            mPrinter.addCut(Printer.CUT_FEED);
+            FirebaseDatabase firebase = FirebaseDatabase.getInstance();
+            String dateStampChild = new SimpleDateFormat("dd").format(new Date());
+            firebase.getReference("estimates").child(dateStampChild).child(timeStamp).setValue(printString.split("_")[1]);
+        } catch (Exception e) {
+            ShowMsg.showException(e, "", mContext);
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean isPrintable(PrinterStatusInfo status) {
+        if (status == null) {
+            return false;
+        }
+
+        if (status.getConnection() == Printer.FALSE) {
+            return false;
+        } else if (status.getOnline() == Printer.FALSE) {
+            return false;
+        } else {
+            ;//print available
+        }
+
+        return true;
+    }
+
+    private boolean printData() {
+        if (mPrinter == null) {
+            return false;
+        }
+
+        if (!connectPrinter()) {
+            return false;
+        }
+
+        PrinterStatusInfo status = mPrinter.getStatus();
+
+
+        if (!isPrintable(status)) {
+            try {
+                mPrinter.disconnect();
+            } catch (Exception ex) {
+                // Do nothing
+            }
+            return false;
+        }
+
+        try {
+            mPrinter.sendData(Printer.PARAM_DEFAULT);
+        } catch (Exception e) {
+            ShowMsg.showException(e, "sendData", mContext);
+            try {
+                mPrinter.disconnect();
+            } catch (Exception ex) {
+                // Do nothing
+            }
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean connectPrinter() {
+        boolean isBeginTransaction = false;
+
+        if (mPrinter == null) {
+            return false;
+        }
+
+        try {
+            mPrinter.connect(PRINTER, Printer.PARAM_DEFAULT);
+        } catch (Exception e) {
+            ShowMsg.showException(e, "connect", mContext);
+            return false;
+        }
+
+        try {
+            mPrinter.beginTransaction();
+            isBeginTransaction = true;
+        } catch (Exception e) {
+            ShowMsg.showException(e, "beginTransaction", mContext);
+        }
+
+        if (isBeginTransaction == false) {
+            try {
+                mPrinter.disconnect();
+            } catch (Epos2Exception e) {
+                // Do nothing
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void finalizeObject() {
+        Log.d(TAG, "finalizeObject: ");
+        if (mPrinter == null) {
+            return;
+        }
+
+        mPrinter.clearCommandBuffer();
+
+        mPrinter.setReceiveEventListener(null);
+
+        mPrinter = null;
+    }
+//
+    @Override
+    public void onPtrReceive(final Printer printerObj, final int code, final PrinterStatusInfo status, final String printJobId) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public synchronized void run() {
+                ShowMsg.showResult(code, makeErrorMessage(status), mContext);
+
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        disconnectPrinter();
+                    }
+                }).start();
+            }
+        });
+    }
+
+    private void disconnectPrinter() {
+        if (mPrinter == null) {
+            return;
+        }
+
+        try {
+            mPrinter.endTransaction();
+        } catch (final Exception e) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public synchronized void run() {
+                    ShowMsg.showException(e, "endTransaction", mContext);
+                }
+            });
+        }
+
+        try {
+            mPrinter.disconnect();
+        } catch (final Exception e) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public synchronized void run() {
+                    ShowMsg.showException(e, "disconnect", mContext);
+                }
+            });
+        }
+
+        finalizeObject();
+    }
+
+    private String makeErrorMessage(PrinterStatusInfo status) {
+        String msg = "";
+
+        if (status.getOnline() == Printer.FALSE) {
+            msg += getString(R.string.handlingmsg_err_offline);
+        }
+        if (status.getConnection() == Printer.FALSE) {
+            msg += getString(R.string.handlingmsg_err_no_response);
+        }
+        if (status.getCoverOpen() == Printer.TRUE) {
+            msg += getString(R.string.handlingmsg_err_cover_open);
+        }
+        if (status.getPaper() == Printer.PAPER_EMPTY) {
+            msg += getString(R.string.handlingmsg_err_receipt_end);
+        }
+        if (status.getPaperFeed() == Printer.TRUE || status.getPanelSwitch() == Printer.SWITCH_ON) {
+            msg += getString(R.string.handlingmsg_err_paper_feed);
+        }
+        if (status.getErrorStatus() == Printer.MECHANICAL_ERR || status.getErrorStatus() == Printer.AUTOCUTTER_ERR) {
+            msg += getString(R.string.handlingmsg_err_autocutter);
+            msg += getString(R.string.handlingmsg_err_need_recover);
+        }
+        if (status.getErrorStatus() == Printer.UNRECOVER_ERR) {
+            msg += getString(R.string.handlingmsg_err_unrecover);
+        }
+        if (status.getErrorStatus() == Printer.AUTORECOVER_ERR) {
+            if (status.getAutoRecoverError() == Printer.HEAD_OVERHEAT) {
+                msg += getString(R.string.handlingmsg_err_overheat);
+                msg += getString(R.string.handlingmsg_err_head);
+            }
+            if (status.getAutoRecoverError() == Printer.MOTOR_OVERHEAT) {
+                msg += getString(R.string.handlingmsg_err_overheat);
+                msg += getString(R.string.handlingmsg_err_motor);
+            }
+            if (status.getAutoRecoverError() == Printer.BATTERY_OVERHEAT) {
+                msg += getString(R.string.handlingmsg_err_overheat);
+                msg += getString(R.string.handlingmsg_err_battery);
+            }
+            if (status.getAutoRecoverError() == Printer.WRONG_PAPER) {
+                msg += getString(R.string.handlingmsg_err_wrong_paper);
+            }
+        }
+        if (status.getBatteryLevel() == Printer.BATTERY_LEVEL_0) {
+            msg += getString(R.string.handlingmsg_err_battery_real_end);
+        }
+
+        return msg;
+    }
+
+    public void testPrinter(View view) {
+        runPrintReceiptSequence("Testing0_20");
+
+    }
 }
